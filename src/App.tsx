664eccc2680ce49c7688dc42f9ec0016ac440cbf
Bryan? <llcbryan@users.noreply.github.com>
2017-11-24 02:43:42 -0800

    Impose maximum item limit

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,37 @@ import { pickCast } from './CastLogic';
 
 import { CastPicker } from './CastPicker';
 
+const MAX_RESULTS = 5;
+
 class App extends React.Component<{}, App.State> {
 
   constructor(props: CastPicker.Props) {
     super(props);
-    this.state = { previousResults: [] };
+    this.state = { generatedResults: [] };
   }
 
   public render() {
     return (
       <CastPicker
-                results={this.state.previousResults}
+                results={this.state.generatedResults}
                 startPick={this.pick}
       />
     );
   }
 
   private pick = () => {
-    this.setState(oldState => ({
-      previousResults: [ pickCast(), ...oldState.previousResults ]
-    }));
+    this.setState(oldState => {
+      let newResults = [ pickCast(), ...oldState.generatedResults ];
+      return {
+        generatedResults: newResults.slice(0, MAX_RESULTS)
+      };
+    });
   }
 }
 
 namespace App {
   export interface State {
-    previousResults: string[];
+    generatedResults: string[];
   }
 }
 
