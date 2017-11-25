6f2b93f24b849bfa19731a71df0e1c98101112df
Bryan? <llcbryan@users.noreply.github.com>
2017-11-24 18:54:49 -0800

    Display multiple casts

--- src/CastList.tsx
+++ src/CastList.tsx
@@ -22,7 +22,25 @@ export interface CastsListItem {
 }
 
 function ListItem(props: ListItem.Props) {
+  if (props.casts.length === 1) {
+    return SingleCastListItem(props);
+  } else {
+    return MultiCastListItem(props);
+  }
+}
+
+function SingleCastListItem(props: ListItem.Props) {
   return <li key={props.key}>{props.casts[0].toString()}</li>;
+}
+
+function MultiCastListItem(props: ListItem.Props) {
+  let phrase = (props.casts.length > 2) ? 'Combo casts!!!' : 'Dual casts!!!';
+  return (
+    <li key={props.key}>
+      {phrase}<br />
+      {props.casts.map((c, i) => <span key={i}>{c.toString()}<br /></span>)}
+    </li>
+  );
 }
 
 namespace ListItem {
