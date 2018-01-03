484e3ee71aa9864e4c4a1caa2a365bdf53c92d26
Bryan? <llcbryan@users.noreply.github.com>
2018-01-02 23:07:09 -0800

    Make the outer container CSS better

--- src/CastPicker.tsx
+++ src/CastPicker.tsx
@@ -3,17 +3,29 @@ import { Button, Container, Header } from 'semantic-ui-react';
 
 import { CastList, CastsListItem } from './CastList';
 
+const FlexContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+};
+
+const AppContainerStyle: React.CSSProperties = {
+  padding: 14,
+  minWidth: 275,
+  maxWidth: 400,
+};
+
 export function CastPicker(props: CastPicker.Props) {
   return (
-    <Container text={true}>
-      <Container textAlign="center">
-        <Header as='h1'>Cast Picker</Header>
-        <Button primary={true} onClick={props.startPick}>Pick</Button>
-      </Container>
-      <Container>
+    <div style={FlexContainerStyle}>
+      <div style={AppContainerStyle}>
+        <Container textAlign="center">
+          <Header as='h1'>Cast Picker</Header>
+          <Button primary={true} onClick={props.startPick}>Pick</Button>
+        </Container>
         <CastList casts={props.generatedCasts} />
-      </Container>
-    </Container>
+      </div>
+    </div>
   );
 }
 
